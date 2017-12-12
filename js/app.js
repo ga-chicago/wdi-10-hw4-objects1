fbbf35f88b550f2cad34f186ae99343d07cf5553
Anthony <anthonyjlower@gmail.com>
2017-12-12 17:23:15 -0600

    created some data structures and accessed some data: done

--- js/app.js
+++ js/app.js
@@ -5,3 +5,23 @@
 // 5. Objects - with strings
 // 6. Objects - with a strings and an array
 
+		// Take it easy
+
+// 1. 
+
+let rainbowColors = ["red", "orange", "yellow", 'green', 'blue', 'indigo', 'violet'];
+
+// 2. 
+
+console.log(rainbowColors[4])
+
+// 3. 
+
+let anthony = {
+	favFood: "tacos",
+	hobbies: ["biking"],
+	currentHometown: "Chicago, IL",
+	favDatatype: "boolean"
+}
+
+console.log(anthony.hobbies[0])
