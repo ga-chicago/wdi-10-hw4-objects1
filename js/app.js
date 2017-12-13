4e00d471a4f7dac9496afe4ffb9e4e38f2262dfe
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-13 14:01:18 -0600

    selecting datatypes done

---
+++ js/app.js
@@ -0,0 +1,70 @@
+console.log('The Chamber of Secrets has been opented. Enemies of the Heir beware.')
+/* 1. A light switch that can be either on or off.
+boolean: light on == true
+
+2. A user's email address.
+string: "me@me.com"
+
+3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+array: let spaceship = ['hull', 'laser blasters', 'tractor beam', 'warp drive']
+
+4. A list of student names from our class.
+array: let studentNames = ['Kait', 'Ryne', 'Travis', 'Sergio', 'Sierra', 'Jackson']
+
+5. A list of student names from our class, each with a location.
+object: let stuNameAndLoc = {
+	Stu1: {
+		name: 'Kait',
+		location: 'St. Louis'
+	}
+
+	Stu2: {
+		name: 'Michael',
+		location: 'Chicago'
+	}
+
+	Stu3: {
+		name: 'Travis',
+		location: 'Portland'
+	}
+
+	Stu4 {
+		name: 'Sergio',
+		location:'Austin'
+	}
+}
+
+6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+object with an array: let stuNameAndLoc = {
+	Stu1: {
+		name: 'Kait'
+		location: 'St. Louis'
+		favMovies: ['Breakfast at Tiffany's', 'Amelie', 'Elizabethtown']
+	}
+
+	Stu2: {
+		name: 'Michael'
+		location: 'Chicago'
+		favMovies: ['Boondock Saints', 'Men In Black', 'Green Street Hooligans']
+	}
+
+	Stu3: {
+		name: 'Travis'
+		location: 'Portland'
+		favMovies: ['Captain America: Civil War', 'Thor: Ragnarok', 'Wonder Woman']
+	}
+
+	Stu4 {
+		name: 'Sergio'
+		location:'Austin'
+		favMovies: ['The Secret Life of Pets', 'Sing', 'Zootopia']
+	}
+}
+*/
+
+
+
+
+
+
+
