3fe570df050e7ff97885cf1e2f5d22a7af8abd40
Michael Weber <webermn15@gmail.com>
2017-12-12 20:39:47 -0600

    selecting datatypes done

---
+++ js/app.js
@@ -0,0 +1,93 @@
+//DATATYPES
+
+//Answer the following
+
+//1. I would use a Boolean data type to represent the data. It would look something like:
+
+// const lightSwitch = {
+// 	flippedUp: true
+// }
+
+//2. I would use a string data type to store an email address.
+
+// const userInfo = {
+// 	email: 'user@userwebsite.com'
+// }
+
+//3. I would use an object with keys that list the attributes of the spaceship and either strings or arrays of strings that describe those attributes
+
+// const spaceShip = {
+// 	hull: "Titanium Plated Hull",
+// 	weapons: ["Laser Blasters", "Tractor Beam"],
+// 	propulsion: "Warp Drive"
+// }
+
+//4. I would use an array containing strings 
+
+// const students = ["Michael", "Somers", "Anthony", "Kait"];
+
+//5. I would use an array of objects containing key value pairs representing their attributes listed in strings
+
+// const classList = [ { 
+// 	name: "Michael",
+// 	location: "classroom one"
+// 	}, {
+// 	name: "Somers",
+// 	location: "kitchen"
+// 	}, {
+// 	name: "Anthony",
+// 	location: "classroom one"
+// 	}, {
+// 	name: "Kait",
+// 	location: "classroom one"
+// 	}
+// ]
+
+//6. I would use an array of objects containting key value pairs representing their attributes listed in strings or strings within an array
+
+const classListTwo = [ { 
+	name: "Michael",
+	location: "classroom one",
+	favoriteShows: ["Breaking Bad", "Parks and Rec"]
+	}, {
+	name: "Somers",
+	location: "kitchen",
+	favoriteShows: ["The Wire", "America's Next Top Chef"]
+	}, {
+	name: "Anthony",
+	location: "classroom one",
+	favoriteShows: ["Game of Thrones", "Star Trek"]
+	}, {
+	name: "Kait",
+	location: "classroom one",
+	favoriteShows: ["Road Truckers", "NFL Pregame Sunday"]
+	}
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
