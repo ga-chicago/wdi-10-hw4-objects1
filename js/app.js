02f95782e76ceb0b6da98c8297acfed7a1b539a0
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-12 18:39:25 -0600

    selecting datatypes done

---
+++ js/app.js
@@ -0,0 +1,25 @@
+console.log("works");
+//ANSWER THE FOLLOWING******************
+//1 a light switch with on/off
+//  I would store it in an object function by using lightSwitch as a key
+// and then function(){// turn on //turn off}
+
+//2 a user's email address
+//  I would store in an object called user with a key of email
+
+//3 A spaceship with 4 things
+// I would create an object called spaceShip and make a key for each item
+// and if the lasers, beam, and warp drive are supposed to do something i
+// would make functions for each of those keys
+
+//4 a list of our classes students
+// I would store in an array variable called students
+
+//5 "" with location
+// I would store in an object called students and for each student I would
+// create objects for each student with a key of name and key of location
+
+
+//6 "" with more.
+// I would add a key favoriteTVshows to my previous student object to each
+// of the students individual objects within the larger student object
