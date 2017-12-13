441d22e6a97183031d1e38b4d9c0196e821dc776
Dare Oni <dareoni@Dares-MacBook-Air.local>
2017-12-13 13:37:26 -0600

    turning in assignment

---
+++ app.js
@@ -0,0 +1,96 @@
+// question 1
+
+// for a light switch statement you would use a if else statemet
+var light = 'bright'
+if(switchup === on){
+	return bright
+} else(swithDown === off){
+	return dark
+}
+
+//question 2
+// for a email you would use object 
+var user {
+	email: 'dare'
+	password: 'ginsing'
+
+}
+
+//question 3
+// you wourld make an variables
+var spaceship = ['hull', 'laserblaster', 'tractor beam', 'warp drive']
+
+/*question 4*/
+
+make an array 
+
+var students = ["jane", "Adam", "lisa", "Mark", "jesse", "allen"]
+
+
+//question 5
+
+would be a object
+
+var students = {
+class: wdi
+location: chicago
+studentName: ['yasmin', 'jeniffer', 'allen']
+}
+
+// question 6
+//var 
+
+var student name: 
+	{ name: "allen", location: "munster",
+	{ name: "bart", location:"chicago"
+	{ name: "jesse", location:"calfornia"
+	{ name: "caleb", location: "wisconsin"
+	{ name: "dare", location: "mexico"
+	{ name: "lisa" location: "las vegas"
+
+//take it easy
+	var rainbow = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'indigo'];
+	console.log(rainbow.pop(blue);
+
+// question 3 and 4
+
+var name = {
+	favoriteFood:"crab",
+	hobby:"drinking",
+	nameOfTown:"musnter"
+	hobby: function() {
+
+	return this.name.hobby;
+	}
+
+}
+
+
+// crazy object
+
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [{
+      name: "Jeff",
+      occupation: "manager"
+    },
+    {
+      name: "funkhauser",
+      occupation: "tv dude"
+    },
+    {
+      name: "susie",
+      occupation: "jeffs wife",
+      favourtieHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
+}
+
