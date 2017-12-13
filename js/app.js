ff739d576d71463f428ff289292d42cea396e1c5
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2017-12-13 16:55:18 -0600

    created some data structures and accessed some data: done

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// 1. A light switch that can be either on or off.
+
+// boolean
+
+// 2. A user's email address.
+
+// a variable
+
+// 3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+
+// an object
+
+// 4. A list of student names from our class.
+
+// an array containing objects 
+
+// 5. A list of student names from our class, each with a location.
+
+// an array containing objects 
+
+// 6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+
+// an array containing objects 
+
+let rainbow = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+console.log(rainbow[4]);
+
+let Jackson = {
+	favoriteFood: "Chicago dog",
+	hobby: "graphic design",
+	town: "Chicago",
+	favoriteDatatype: "objects"
+}
+
+console.log(Jackson.hobby);
