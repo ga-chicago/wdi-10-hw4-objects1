41c387279bdb1f97ef149061cffac0ed02176d9f
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-12 19:23:05 -0600

    performed operations on the crazy object

--- js/app.js
+++ js/app.js
@@ -25,13 +25,72 @@ console.log("works");
 // of the students individual objects within the larger student object
 
 //******************TAKE IT EASY****************************
-let rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-console.log(rainbowColors[4]);
+// let rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+// console.log(rainbowColors[4]);
+//
+// let sierra = {
+//     favFood: "enchiladas",
+//     hobby: "guitar",
+//     lives: "chicago",
+//     favDataType: "objects"
+// };
+// console.log(sierra.hobby);
 
-let sierra = {
-    favFood: "enchiladas",
-    hobby: "guitar",
-    lives: "chicago",
-    favDataType: "objects"
+//****************CRAZY OBJECT********************************
+const crazyObject = {
+    taco: [{meat: 'steak',cheese: ['panela', 'queso', 'chihuahua']},
+        {meat: 'chicken', salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+    ],
+    larry: {
+        nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+        quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+        characters: [{
+            name: "Jeff",
+            occupation: "manager"
+        },
+            {
+                name: "funkhauser",
+                occupation: "tv dude"
+            },
+            {
+                name: "susie",
+                occupation: "jeffs wife",
+                favourtieHobby: "Swearing at Larry and Jeff"
+            },
+        ]
+    }
 };
-console.log(sierra.hobby);
+
+//1
+console.log(crazyObject.taco[1].salsa[5]);
+//2
+console.log(crazyObject.larry.quotes[0]);
+//3
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+//4
+console.log(crazyObject.larry.nicknames[1]);
+//5
+console.log(crazyObject.larry.characters[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
