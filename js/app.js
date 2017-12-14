43ad8014d55944b06726dfcdcce04e7b9dd70081
Somers Matthews <somersbmatthews@uchicago.edu>
2017-12-13 20:38:25 -0600

    Change the value of  to

---
+++ js/app.js
@@ -0,0 +1,91 @@
+//  1. A light switch that can be either on or off.
+    // boolean
+//  2. A user's email address.
+        string
+//  3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+        object
+//  4. A list of student names from our class.
+        array
+//  5. A list of student names from our class, each with a location.
+        object
+//  6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+        object
+
+
+// 1. Make an array that holds all of the colors of the rainbow.
+  const arr = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+// 2. Write code that will access "blue" from the rainbow array.
+  arr[4];
+// 3. Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+  var Somers = {
+    name: 'Somers'
+    favorite_food: 'sushi'
+    hobby: 'piano'
+    town: 'Chicago'
+    favorite_datatype: 'array'
+  }
+// 4. Write code that will access your hobby from the object that you just created.
+Somers.hobby
+
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+          nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+          quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+          characters: [
+                       {
+                     name: "Jeff",
+                      occupation: "manager"
+                       },
+                       {
+                      name: "funkhauser",
+                      occupation: "tv dude"
+                       },
+                      {
+                      name: "susie",
+                      occupation: "jeffs wife",
+                      favourtieHobby: "Swearing at Larry and Jeff"
+                      },
+                       ]
+          }
+}
+
+      // Use crazy Object to log the following.
+
+      // 1. "omg my mouth is burning"
+      crazyObject.taco[1].salsa[5];
+      // 2. "Pretty pretty prettayyyyy good"
+      crazyObject.taco
+      // 3. "Swearing at Larry and Jeff"
+      crazyObject.larry.characters[2].favourtieHobby
+      // 4. "Chicken Teriyaki Boyyyyyy"
+      crazyObject.larry.nicknames[1]
+      // 5. The object the contains the name `funkhauser`
+      crazyObject.larry.characters[1].name
+
+      const inception = {
+         reality: {
+             dreamLayer1: {
+                 dreamLayer2: {
+                     dreamLayer3: {
+                         dreamLayer4: {
+                             dreamLayer5: {
+                                 dreamLayer6: {
+                                     limbo: "Joseph Gordon Levitt"
+                                }
+                             }
+                         }
+                     }
+                 }
+             }
+         }
+      }
+
+// Change the value of `limbo` to `null`.
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null
+
+      
