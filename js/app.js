8f383bbd1cddd7101c15233852d738004d0fc4e3
sergio <scrummish@gmail.com>
2017-12-13 21:05:07 -0600

    accessed limbo

--- js/app.js
+++ js/app.js
@@ -99,6 +99,25 @@ crazyObject.larry.nicknames[1];
 crazyObject.larry.characters[1];
 
 
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
 
 
 
