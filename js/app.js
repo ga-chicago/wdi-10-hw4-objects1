e4bf3877f65ed5b11ebcd38675fe8b59fe644c9d
Somers Matthews <somersbmatthews@uchicago.edu>
2017-12-13 23:57:31 -0600

    homework, sick

--- js/app.js
+++ js/app.js
@@ -1,27 +1,38 @@
+// tired and sick, homework abilities reduced. this is a few minutes late prolly, I didn't
+// comment Enough
+
+// also, I don't understand the sort thing fully.
+
+// i can do another pull request later.
+
+
+
+
 //  1. A light switch that can be either on or off.
     // boolean
 //  2. A user's email address.
-        string
+    //    string
 //  3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
-        object
+    //    object
 //  4. A list of student names from our class.
-        array
+    //    array
 //  5. A list of student names from our class, each with a location.
-        object
+    //    object
 //  6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
-        object
+      //  object
 
 
 // 1. Make an array that holds all of the colors of the rainbow.
   const arr = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 // 2. Write code that will access "blue" from the rainbow array.
   arr[4];
+  console.log(arr[4])
 // 3. Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
   var Somers = {
-    name: 'Somers'
-    favorite_food: 'sushi'
-    hobby: 'piano'
-    town: 'Chicago'
+    name: 'Somers',
+    favorite_food: 'sushi',
+    hobby: 'piano',
+    town: 'Chicago',
     favorite_datatype: 'array'
   }
 // 4. Write code that will access your hobby from the object that you just created.
@@ -88,4 +99,71 @@ const crazyObject = {
 // Change the value of `limbo` to `null`.
 inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null
 
-      
+
+
+const bondFilms = [
+  { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
+  { "title" : "Thunderball", "year" : 1965, "actor" : "Sean Connery", "gross" : "$1,014,941,117" },
+  { "title" : "Goldfinger", "year" : 1964, "actor" : "Sean Connery", "gross" : "$912,257,512" },
+  { "title" : "Live and Let Die", "year" : 1973, "actor" : "Roger Moore", "gross" : "$825,110,761" },
+  { "title" : "You Only Live Twice", "year" : 1967, "actor" : "Sean Connery", "gross" : "$756,544,419" },
+  { "title" : "The Spy Who Loved Me", "year" : 1977, "actor" : "Roger Moore", "gross" : "$692,713,752" },
+  { "title" : "Casino Royale", "year" : 2006, "actor" : "Daniel Craig", "gross" : "$669,789,482" },
+  { "title" : "Moonraker", "year" : 1979, "actor" : "Roger Moore", "gross" : "$655,872,400" },
+  { "title" : "Diamonds Are Forever", "year" : 1971, "actor" : "Sean Connery", "gross" : "$648,514,469" },
+  { "title" : "Quantum of Solace", "year" : 2008, "actor" : "Daniel Craig", "gross" : "$622,246,378" },
+  { "title" : "From Russia with Love", "year" : 1963, "actor" : "Sean Connery", "gross" : "$576,277,964" },
+  { "title" : "Die Another Day", "year" : 2002, "actor" : "Pierce Brosnan", "gross" : "$543,639,638" },
+  { "title" : "Goldeneye", "year" : 1995, "actor" : "Pierce Brosnan", "gross" : "$529,548,711" },
+  { "title" : "On Her Majesty's Secret Service", "year" : 1969, "actor" : "George Lazenby", "gross" : "$505,899,782" },
+  { "title" : "The World is Not Enough", "year" : 1999, "actor" : "Pierce Brosnan", "gross" : "$491,617,153" },
+  { "title" : "For Your Eyes Only", "year" : 1981, "actor" : "Roger Moore", "gross" : "$486,468,881" },
+  { "title" : "Tomorrow Never Dies", "year" : 1997, "actor" : "Pierce Brosnan", "gross" : "$478,946,402" },
+  { "title" : "The Man with the Golden Gun", "year" : 1974, "actor" : "Roger Moore", "gross" : "$448,249,281" },
+  { "title" : "Dr. No", "year" : 1962, "actor" : "Sean Connery", "gross" : "$440,759,072" },
+  { "title" : "Octopussy", "year" : 1983, "actor" : "Roger Moore", "gross" : "$426,244,352" },
+  { "title" : "The Living Daylights", "year" : 1987, "actor" : "Timothy Dalton", "gross" : "$381,088,866" },
+  { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
+  { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
+];
+
+ // Create a new array called `bondTitles` with only the titles of the Bond films, and console.log the new array.
+//
+
+ for(let i in bondFilms) {
+console.log(bondFilms[i].title);
+ }
+// Create a new array `oddBonds`, of only the Bond films released on odd-numbered years.
+let arrayOfTitles = []
+for(let i in bondFilms) {
+
+  let year = bondFilms[i].year;
+
+  if(year%2===1){
+  arrayOfTitles.push(bondFilms[i].title);
+  }
+}
+console.log(arrayOfTitles);
+// Determine the total cumulative gross of the Bond franchise, and console.log the result. _hint_ To make the grosses into usable numbers, look into the `.replace` Javascript method (there are many ways to do this, however). Look into `parseInt` also.
+let str = "";
+let num = 0;
+let sum = 0;
+for(let i in bondFilms){
+str = bondFilms[i].gross
+str = str.replace(/,/g, "")
+str = str.substring(1)
+
+num = parseInt(str)
+sum = sum + num
+}
+console.log(sum + " damn dollars");
+
+let arrayOfActors = []
+for(let i in bondFilms) {
+  arrayOfActors.push(bondFilms[i].actor)
+}
+
+// too tired and sick to do homework, stopping now
+}
+
+{ "title" : "On Her Majesty's Secret Service", "year" : 1969, "actor" : "George Lazenby", "gross" : "$505,899,782" }
