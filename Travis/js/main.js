78013d8c963d679a1e76752d99f47f17e3869b68
Travis1282 <tmail20@gmail.com>
2017-12-13 20:57:07 -0600

    selecting datatypes done.

---
+++ Travis/js/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+// A light switch that can be either on or off. Numbers
+// A user's email address. Strings
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive. Objects
+// A list of student names from our class. Strings
+// A list of student names from our class, each with a location. Arrays
+// A list of student names from our class, each with a location and each with a list of favorite tv shows. Booleans
+
+let rainbow = ["blue","red","green","yellow"]
+console.log(rainbow[0]);
