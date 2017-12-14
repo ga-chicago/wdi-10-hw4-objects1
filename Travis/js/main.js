485f08d70d76b835d2afeaa2285d7325cbd502ea
Travis1282 <tmail20@gmail.com>
2017-12-13 22:09:05 -0600

    created some data structures and accessed some data: done

--- Travis/js/main.js
+++ Travis/js/main.js
@@ -2,12 +2,77 @@
 
 
 
-// A light switch that can be either on or off. Numbers
-// A user's email address. Strings
-// A spaceship with a hull, laser blasters, tractor beam, and warp drive. Objects
+// A light switch that can be either on or off. Booleans
+
+let light = false;
+
+// A user's email address. Objects  
+ 
+let email = { 
+	name: 'unknown',
+	email: 'email@unknown.com'
+}
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive. String
+
+let spaceship = ('hull', 'laser blasters', 'tractor beam', 'warp drive')
+
+
 // A list of student names from our class. Strings
-// A list of student names from our class, each with a location. Arrays
-// A list of student names from our class, each with a location and each with a list of favorite tv shows. Booleans
+let wdi10 = ('kait', 'charls', 'michael')
+
+// A list of student names from our class, each with a location. array of Objects
+
+let wdi10_2 = [
+	{name:'kait', location: 'St.Louis'},
+	{name:'charls', location: 'south side'},
+	{name:'michael', location: 'north side'}
+]
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows. array of Objects
+ 
+ let wdi10_3 = [
+	{name:'kait', location: 'St.Louis', tvShows: 'orange is the new black'},
+	{name:'charls', location: 'south side', tvShows: 'attack on titan'},
+	{name:'michael', location: 'north side', tvShows: 'breaking bad'}
+]
+
+// Make an array that holds all of the colors of the rainbow.
+// Write code that will access "blue" from the rainbow array.
+
 
 let rainbow = ["blue","red","green","yellow"]
 console.log(rainbow[0]);
+
+
+
+// Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype. Write code that will access your hobby from the object that you just created.
+
+let yourName = {
+	favFood: 'mayo',// what?!
+	hobby: 'beekeeping', 
+	town: 'hermosa town',
+	datatype: 'social'
+}
+
+console.log(yourName.datatype)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
