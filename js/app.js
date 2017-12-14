1b2c9863d852e2a4b51f3209b580b79df784dfc1
Charles <because@charlesis.me>
2017-12-14 07:43:36 -0600

    Refresher

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// 1. boolean - there are only true states
+// 2. string - does it contain @?
+// 3. Objects - there are a lot of working parts to one thing
+// 4. Array - A lot of things with no detail
+// 5. Objects - there are a lot of working parts to one thing, with more detail.
+// 6. Objects - there are a lot of working parts to one thing, with even more detail.
