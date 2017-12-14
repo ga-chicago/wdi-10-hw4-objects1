45c1adabda0391a6cbf2098aed31b0564cf3dba4
ethan375 <ethanebel1@gmail.com>
2017-12-13 21:43:24 -0600

    created some data structures and accessed some data: done

---
+++ js/app.js
@@ -0,0 +1,72 @@
+//1 
+"A light switch would be most like a boolean. A light switch is either on or off just like a boolean is true or flase. there is no middle ground." "lightbulb on" = true
+
+//2
+"An email is most like a string, it is just a line of text." 
+
+//3
+ "a spaceship is most like an object ex":
+
+let spaceShip = {
+
+hull: {
+
+weapon: "laser blaster",
+
+extractionUtility: "tractor beam",
+
+engines: "warp dirve"
+
+}
+
+}
+
+
+//4
+  A student names list would be most similar to an array filled with strings ["ethan", "",]etc
+
+//5 
+"a list of student names with location would be like an object"
+let attendance = {
+  ethan: "home",
+  ryne: "GA"
+};
+
+//6
+"A list of student names with a location and favorite tv show would be like an object nested in an array"
+let attendance = [
+  {name:'ethan', location: 'GA', favoriteTvShow:'Game of thrones'}]
+
+//Take it easy
+
+
+
+//1
+
+let rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+
+//2
+
+rainbowColors[4]
+
+
+//3
+
+let ethan = {
+
+favoriteFood: "bananas",
+
+hobby: "collecting rare coins",
+
+currentTown: "palos park",
+
+favoriteDatatype: "objects"
+
+}
+
+
+//4
+
+ethan.hobby
+
