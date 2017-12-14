2df487c8a54e34828a127461e3203f201e7eb5f2
Charles <because@charlesis.me>
2017-12-14 07:48:38 -0600

    selecting datatypes done

--- js/app.js
+++ js/app.js
@@ -4,3 +4,17 @@
 // 4. Array - A lot of things with no detail
 // 5. Objects - there are a lot of working parts to one thing, with more detail.
 // 6. Objects - there are a lot of working parts to one thing, with even more detail.
+
+<!-- //Take it easy -->
+    
+    
+
+let rainbowColors = ["red", "blue", "yellow", 'orange', 'green', 'purple'];
+
+console.log(rainbowColors[1])
+
+let anthony = {
+	favFood: "Broccoli",
+	hobbies: ["Video games"],
+	currentHometown: "Chicago",
+	favDatatype: "boolean"
